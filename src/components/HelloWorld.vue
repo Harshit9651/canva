df0a0a80b7f9076a83a9ac5440eb133456e455e6
Harshit sharma <programming966051@gmail.com>
2024-10-11 18:28:29 +0530

    issuing some image problem

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,39 @@
 <template>
-    <div>
-      <h1>Fabric.js</h1>
+  <div>
+    <h1>Fabric.js</h1>
 
-      <canvas ref="canvas" width="600" height="400"></canvas>
-    </div>
-  </template>
-  
-  <script setup>
-  import { onMounted, ref } from 'vue';
-  import * as fabric from 'fabric';
- 
-  const canvasRef = ref(null);
-  
-  onMounted(() => {
-    
-    const canvas = new fabric.Canvas(canvasRef.value);
-  
-    
-    fabric.Image.fromURL('https://images.pexels.com/photos/271649/pexels-photo-271649.jpeg?auto=compress&cs=tinysrgb&w=600', function (img) {
+    <canvas ref="canvas"></canvas>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import * as fabric from "fabric";
+// import { fabric } from 'fabric';
+
+const canvasRef = ref(null);
+
+onMounted(() => {
+  const canvas = new fabric.Canvas(canvasRef.value);
+  fabric.Image.fromURL(
+    "https://images.pexels.com/photos/271649/pexels-photo-271649.jpeg?auto=compress&cs=tinysrgb&w=600",
+    function (img) {
       img.scale(0.5);
       canvas.add(img);
       canvas.renderAll();
-    });
-  });
-  </script>
-  
-  <style lang="scss" scoped>
-  canvas {
-    border: 1px solid #ccc;
-    height: 30.75rem;
-    width: 30.5rem;
-    
-  }
-  </style>
-  
+    }
+  );
+});
+</script>
+
+<style lang="scss" scoped>
+canvas {
+  border: 1px solid #ccc;
+  height: 30.75rem;
+  width: 30.5rem;
+  border-radius: 1rem;
+  -webkit-box-shadow: 1px -4px 15px -1px rgba(110, 62, 110, 1);
+  -moz-box-shadow: 1px -4px 15px -1px rgba(110, 62, 110, 1);
+  box-shadow: 1px -4px 15px -1px rgba(110, 62, 110, 1);
+}
+</style>
